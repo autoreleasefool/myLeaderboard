2b9d1a681b05406d76a302c3a25a9d527c92c063
Joseph Roque <contact@josephroque.ca>
2019-06-27 23:13:11 -0400

    Render player avatar

---
+++ src/components/PlayerView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Player } from '../utils/Octo';
+import './PlayerView.css';
+
+interface Props {
+    player: Player;
+}
+
+function PlayerView(props: Props) {
+    const { player } = props;
+    return <img src={player.avatar} alt={player.displayName} />;
+}
