c368f687e539fa4d2d32eeee6bc9cc11f5c44e3d
Joseph Roque <contact@josephroque.ca>
2019-07-01 20:14:28 -0400

    Clean up

--- api/src/games/router.ts
+++ api/src/games/router.ts
@@ -2,10 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/new', (req, res) => {
-
-});
-
 export default function applyRouter(app: express.Express) {
     app.use('/games', router);
 }
--- api/src/standings/router.ts
+++ api/src/standings/router.ts
@@ -2,10 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/record', (req, res) => {
-
-});
-
 export default function applyRouter(app: express.Express) {
     app.use('/standings', router);
 }
