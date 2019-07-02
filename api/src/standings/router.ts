cc34c48dd5f54deaca4d9c2d4676ccad54f044f1
Joseph Roque <contact@josephroque.ca>
2019-07-02 09:22:55 -0400

    Record game API endpoint

--- api/src/standings/router.ts
+++ api/src/standings/router.ts
@@ -1,6 +1,15 @@
 import express from 'express';
+import record from './record';
 
 const router = express.Router();
+
+router.post('/record', (req, res, next) => {
+    record(req).then(() => {
+        res.sendStatus(200);
+    }).catch(error => {
+        next(error);
+    });
+});
 
 export default function applyRouter(app: express.Express) {
     app.use('/standings', router);
