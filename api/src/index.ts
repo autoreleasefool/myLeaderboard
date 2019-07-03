028f02ff81efba880840d87b054a1aa628687f68
Joseph Roque <contact@josephroque.ca>
2019-07-02 23:22:47 -0400

    Additional API request logging

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,22 @@
 import express, { NextFunction, Request, Response } from 'express';
 import Octo from './lib/Octo';
 
+// tslint:disable:no-console
+
+// Print out startup time to default logs
+console.log('--------------------');
+console.log('Starting new instance of server.');
+console.log(new Date());
+console.log('--------------------');
+
+// Print out startup time to error
+console.error('--------------------');
+console.error('Starting new instance of server.');
+console.error(new Date());
+console.error('--------------------');
+
+// tslint:enable:no-console
+
 const githubToken = process.env.GITHUB_TOKEN;
 if (githubToken != null && githubToken.length > 0) {
     Octo.setToken(githubToken);
@@ -10,6 +26,14 @@ const app = express();
 const port = (process.env.NODE_ENV === 'production') ? 80 : 3001;
 
 app.use(express.json());
+
+// Log each request made to the server
+app.use((req, _, next) => {
+    // tslint:disable:no-console
+    console.log(`${new Date().toISOString()}: ${req.method} from ${req.ip} -- ${req.originalUrl}`);
+    next();
+    // tslint:enable:no-console
+  });
 
 function errorHandler(err: any, _: Request, res: Response, __: NextFunction) {
     // tslint:disable no-console
