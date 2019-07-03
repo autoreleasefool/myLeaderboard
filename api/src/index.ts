56838c739514d0ca7249d6edc204e63dba8beed8
Joseph Roque <contact@josephroque.ca>
2019-07-03 08:00:19 -0400

    Module to explicitly import dotenv

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,5 @@
+import './env';
+
 import express, { NextFunction, Request, Response } from 'express';
 import Octo from './lib/Octo';
 
--- common/types.d.ts
+++ common/types.d.ts
@@ -1,4 +1,5 @@
 declare module 'octokat';
+declare module 'dotenv';
 
 // Players
 
