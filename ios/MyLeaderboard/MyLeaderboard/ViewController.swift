a542dc450c863dc9528b4fa60b98c61beaf51bbe
Joseph Roque <contact@josephroque.ca>
2019-07-06 11:37:29 -0400

    Initial iOS project commit

---
+++ ios/MyLeaderboard/MyLeaderboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLeaderboard
+//
+//  Created by Joseph Roque on 2019-07-06.
+//  Copyright © 2019 Joseph Roque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
