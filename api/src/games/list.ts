dbeb121d1f08aa900eec708c220231d6e1c6f14d
Joseph Roque <contact@josephroque.ca>
2019-07-09 07:21:45 -0400

    Return images with games from API

--- api/src/games/list.ts
+++ api/src/games/list.ts
@@ -1,5 +1,13 @@
-import { allGames } from '../lib/Game';
+import { allGames, Game } from '../lib/Game';
 
-export default async function list(): Promise<Array<string>> {
-    return allGames();
+export default async function list(): Promise<Array<Game>> {
+    const gameNames = await allGames();
+    const games = gameNames.map(name => {
+        return {
+            imageURL: `${process.env.API_URL}/img/games/${name}.png`,
+            name,
+        };
+    });
+
+    return games;
 }
