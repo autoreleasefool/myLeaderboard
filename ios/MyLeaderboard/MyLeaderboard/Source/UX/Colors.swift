3733d8e440bf2ad9db4028022c31fe90de181746
Joseph Roque <contact@josephroque.ca>
2019-07-09 23:16:35 -0400

    UX colors and sizes

---
+++ ios/MyLeaderboard/MyLeaderboard/Source/UX/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  MyLeaderboard
+//
+//  Created by Joseph Roque on 2019-07-09.
+//  Copyright © 2019 Joseph Roque. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	static let background = UIColor(red: 33.0 / 255, green: 43.0 / 255, blue: 53.0 / 255, alpha: 1.0)
+	static let text = UIColor(red: 244.0 / 255, green: 246.0 / 255, blue: 248.0 / 255, alpha: 1.0)
+	static let selection = UIColor(red: 7.0 / 255, green: 97.0 / 255, blue: 187.0 / 255, alpha: 1.0)
+}
