a87ac4cfe5f835015ac847cf777a5771e0db01e5
Joseph Roque <contact@josephroque.ca>
2019-08-06 08:38:25 -0400

    GamePicker

---
+++ ios/MyLeaderboard/MyLeaderboard/Source/Common/Picker/GamePickerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GamePickerViewController.swift
+//  MyLeaderboard
+//
+//  Created by Joseph Roque on 2019-08-06.
+//  Copyright © 2019 Joseph Roque. All rights reserved.
+//
+
+import Foundation
+
+struct GameListQueryable: PickerItemQueryable {
+	func query(api: LeaderboardAPI, completion: @escaping (LeaderboardAPIResult<[Game]>) -> Void) {
+		api.games(completion: completion)
+	}
+}
+
+typealias GamePicker = BasePickerViewController<Game, GameListItemState, GameListQueryable>
+
+class GamePickerViewController: GamePicker {
+	init(api: LeaderboardAPI, multiSelect: Bool = true, initiallySelected: Set<ID>, completion: @escaping GamePicker.FinishedSelection) {
+		let queryable = GameListQueryable()
+		super.init(api: api, initiallySelected: initiallySelected, multiSelect: multiSelect, queryable: queryable, completion: completion)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	override func renderItems(_ items: [Game]) -> [PickerItem<GameListItemState>] {
+		return items.map {
+			return PickerItem(
+				id: $0.id,
+				state: GameListItemState(game: $0)
+			)
+		}
+	}
+}
