42e656d7fcdefe588e5b3437619da1f83a86cc9f
Joseph Roque <joseph.roque@shopify.com>
2019-08-21 18:28:38 -0400

    Bump patch version

--- dashboard/src/components/Version.tsx
+++ dashboard/src/components/Version.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const majorVersion = 5;
 const minorVersion = 0;
-const patchVersion = 0;
+const patchVersion = 1;
 
 function Version() {
     return <p>{`v${majorVersion}.${minorVersion}.${patchVersion}`}</p>;
