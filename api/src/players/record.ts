b3fb749a375534b8c32cdb2de2884fad689336c3
Joseph Roque <contact@josephroque.ca>
2019-08-22 23:08:15 -0400

    Highlight scores for player standings

--- api/src/players/record.ts
+++ api/src/players/record.ts
@@ -3,7 +3,7 @@ import { checkCache } from '../common/utils';
 import Games from '../db/games';
 import Players from '../db/players';
 import Plays from '../db/plays';
-import { PlayerStandings } from '../lib/types';
+import { PlayerStandings, Record } from '../lib/types';
 
 let cacheFreshness = new Date();
 const cachedStandings: Map<number, PlayerStandings> = new Map();
@@ -12,6 +12,13 @@ enum GameResult {
     WON,
     LOST,
     TIED,
+}
+
+interface RecordHighlight {
+    player: number | undefined;
+    winRate: number;
+    losses: number;
+    wins: number;
 }
 
 export default async function record(req: Request): Promise<PlayerStandings> {
@@ -101,6 +108,84 @@ export default async function record(req: Request): Promise<PlayerStandings> {
         };
     }
 
+    highlightRecords(playerRecord);
+
     cachedStandings.set(key, playerRecord);
     return playerRecord;
+}
+
+function highlightRecords(standings: PlayerStandings) {
+    const worstVsRecords: Array<RecordHighlight> = [{ player: undefined, winRate: Infinity, losses: 0, wins: 0 }];
+    const bestVsRecords: Array<RecordHighlight> = [{ player: undefined, winRate: -Infinity, losses: 0, wins: 0 }];
+    const opponentIds: Array<number> = [];
+    for (const recordAgainstOpponent in standings.records) {
+        if (standings.records.hasOwnProperty(recordAgainstOpponent)) {
+            opponentIds.push(parseInt(recordAgainstOpponent, 10));
+        }
+    }
+
+    for (const opponentId of opponentIds) {
+        const vsRecord = standings.records[opponentId];
+        if (vsRecord == null) {
+            continue;
+        }
+
+        const vsTotalGames = vsRecord.wins + vsRecord.losses + vsRecord.ties;
+        const vsWinRate = Math.floor(vsRecord.wins / vsTotalGames * 100);
+
+        const vsRecordForHighlight = { player: opponentId, winRate: vsWinRate, losses: vsRecord.losses, wins: vsRecord.wins };
+        updateHighlightedRecords(vsRecordForHighlight, bestVsRecords, worstVsRecords);
+    }
+
+    const playerVs: Map<number, Record> = new Map();
+    for (const opponentId of opponentIds) {
+        playerVs.set(opponentId, standings.records[opponentId]);
+    }
+
+    markBestAndWorstRecords(playerVs, bestVsRecords, worstVsRecords);
+}
+
+function updateHighlightedRecords(recordHighlight: RecordHighlight, bestRecords: Array<RecordHighlight>, worstRecords: Array<RecordHighlight>) {
+    if (recordHighlight.winRate > bestRecords[0].winRate) {
+        bestRecords.length = 0;
+        bestRecords.push(recordHighlight);
+    } else if (recordHighlight.winRate === bestRecords[0].winRate) {
+        if (recordHighlight.wins > bestRecords[0].wins) {
+            bestRecords.length = 0;
+            bestRecords.push(recordHighlight);
+        } else if (recordHighlight.wins === bestRecords[0].wins) {
+            bestRecords.push(recordHighlight);
+        }
+    }
+    if (recordHighlight.winRate < worstRecords[0].winRate) {
+        worstRecords.length = 0;
+        worstRecords.push(recordHighlight);
+    } else if (recordHighlight.winRate === worstRecords[0].winRate) {
+        if (recordHighlight.losses > worstRecords[0].losses) {
+            worstRecords.length = 0;
+            worstRecords.push(recordHighlight);
+        } else if (recordHighlight.losses === worstRecords[0].losses) {
+            worstRecords.push(recordHighlight);
+        }
+    }
+}
+
+function markBestAndWorstRecords(records: Map<number, Record>, bestRecords: Array<RecordHighlight>, worstRecords: Array<RecordHighlight>) {
+    for (const player of records.keys()) {
+        const playerRecord = records.get(player)!;
+
+        for (const best of bestRecords) {
+            if (best.player === player) {
+                playerRecord.isBest = true;
+                break;
+            }
+        }
+
+        for (const worst of worstRecords) {
+            if (worst.player === player) {
+                playerRecord.isWorst = true;
+                break;
+            }
+        }
+    }
 }
