c13132f8b07326348eec3804928ea7d7bfd0ecd0
Joseph Roque <contact@josephroque.ca>
2019-09-28 01:31:27 +0200

    Bump  version

---
+++ site/precache-manifest.3a499d001078487bbdb4a7475ed35829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305dc975e807d7ecef8e5c41310911b2",
+    "url": "/myLeaderboard/site/index.html"
+  },
+  {
+    "revision": "9abdb7542bd7b47b61ce",
+    "url": "/myLeaderboard/site/static/css/main.8121252b.chunk.css"
+  },
+  {
+    "revision": "ec85a1bdef7cc115ba8e",
+    "url": "/myLeaderboard/site/static/js/2.1e91ff74.chunk.js"
+  },
+  {
+    "revision": "9abdb7542bd7b47b61ce",
+    "url": "/myLeaderboard/site/static/js/main.a8e49fbb.chunk.js"
+  },
+  {
+    "revision": "8904353acadadd9f5fa2",
+    "url": "/myLeaderboard/site/static/js/runtime~main.95da323b.js"
+  }
+]);
