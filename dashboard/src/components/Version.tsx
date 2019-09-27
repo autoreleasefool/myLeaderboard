56f5ff8219802e42640fd623ee049bea3df39fb8
Joseph Roque <contact@josephroque.ca>
2019-09-28 01:30:16 +0200

    Bump dashboard and app version

--- dashboard/src/components/Version.tsx
+++ dashboard/src/components/Version.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const majorVersion = 5;
 const minorVersion = 0;
-const patchVersion = 1;
+const patchVersion = 2;
 
 function Version() {
     return <p>{`v${majorVersion}.${minorVersion}.${patchVersion}`}</p>;
