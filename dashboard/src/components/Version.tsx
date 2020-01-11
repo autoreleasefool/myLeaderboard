1e899b2ae3acd7a82fbed14c3b4742ce38ea2e32
Joseph Roque <contact@josephroque.ca>
2020-01-11 16:55:22 +0100

    Version 5.0.4

--- dashboard/src/components/Version.tsx
+++ dashboard/src/components/Version.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const majorVersion = 5;
 const minorVersion = 0;
-const patchVersion = 3;
+const patchVersion = 4;
 
 function Version() {
     return <p>{`v${majorVersion}.${minorVersion}.${patchVersion}`}</p>;
