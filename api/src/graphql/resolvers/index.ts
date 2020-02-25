51177626335c9dece74b310155fe674fc40c3397
Joseph Roque <contact@josephroque.ca>
2020-02-25 23:38:33 +0100

    Add missing resolvers

--- api/src/graphql/resolvers/index.ts
+++ api/src/graphql/resolvers/index.ts
@@ -3,13 +3,19 @@ import {
     resolvePlayers as players,
     resolvePlayerRecord as playerRecord,
 } from './players';
-import { resolveGames as games } from './games';
+import {
+    resolveGames as games,
+    resolveGameStandings as gameStandings
+} from './games';
+import { resolveHasAnyUpdates as hasAnyUpdates } from './misc';
 
 const resolvers = {
     DateTime: GraphQLDateTime,
     players,
     playerRecord,
     games,
+    gameStandings,
+    hasAnyUpdates,
 };
 
 export default resolvers;
