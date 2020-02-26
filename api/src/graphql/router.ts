317502bc2f44ec395bac5095805bc432816cc579
Joseph Roque <contact@josephroque.ca>
2020-02-26 21:06:35 +0100

    Introspection query

--- api/src/graphql/router.ts
+++ api/src/graphql/router.ts
@@ -1,14 +1,27 @@
 import express from 'express';
+import {graphql, getIntrospectionQuery} from 'graphql';
 import graphQLHTTP from 'express-graphql';
 import schema from './schema';
 import resolvers from './resolvers';
 
-const router = graphQLHTTP({
+const graphQL = graphQLHTTP({
     schema,
     graphiql: true,
     rootValue: resolvers,
 });
 
+const router = express.Router();
+
+router.get('/graphql/introspection', (_, res, next) => {
+    let query = getIntrospectionQuery();
+    graphql(schema, query, resolvers).then(introspection => {
+        res.json(introspection);
+    }).catch(error => {
+        next(error);
+    });
+})
+
 export default function applyRouter(app: express.Express): void {
-    app.use('/graphql', router);
+    app.use(router);
+    app.use('/graphql', graphQL);
 }
