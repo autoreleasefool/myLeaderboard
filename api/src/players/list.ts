d0fd7edfda0494033ad2cdeba921c2ca876fc650
Joseph Roque <contact@josephroque.ca>
2020-02-28 13:21:04 +0100

    Use dataloader for REST players.list

--- api/src/players/list.ts
+++ api/src/players/list.ts
@@ -1,6 +1,13 @@
-import Players from '../db/players';
 import { Player } from '../lib/types';
+import { getListParams, isPlayer } from '../common/utils';
+import DataLoader from '../graphql/dataloader';
+import { Request } from 'express';
+import Players from '../db/players';
 
-export default async function list(): Promise<Array<Player>> {
-    return Players.getInstance().allWithAvatars({});
+export default async function list(req: Request): Promise<Array<Player>> {
+    const [first, offset] = getListParams(req);
+    const loader = DataLoader();
+
+    const players = await loader.playerLoader.loadMany(Players.getInstance().allIds({first, offset}).map(id => String(id)));
+    return players.filter(player => isPlayer(player)).map(player => player as Player)
 }
