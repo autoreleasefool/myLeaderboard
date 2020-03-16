fcfe6fe8532cab1cdad0e2de686264f5b06df5af
Joseph Roque <contact@josephroque.ca>
2020-03-16 09:41:38 +0100

    Minor site version bump

--- dashboard/src/components/Version.tsx
+++ dashboard/src/components/Version.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const majorVersion = 5;
 const minorVersion = 0;
-const patchVersion = 4;
+const patchVersion = 5;
 
 function Version(): React.ReactElement {
     return <p>{`v${majorVersion}.${minorVersion}.${patchVersion}`}</p>;
