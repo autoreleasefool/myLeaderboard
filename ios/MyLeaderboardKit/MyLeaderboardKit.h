dd72f130396108c85f645c0d02c13043bd436124
Joseph Roque <contact@josephroque.ca>
2020-03-25 10:24:56 +0100

    Import dependencies through framework to resolve dynamic lib issue

---
+++ ios/MyLeaderboardKit/MyLeaderboardKit.h
@@ -0,0 +1,19 @@
+//
+//  MyLeaderboardKit.h
+//  MyLeaderboardKit
+//
+//  Created by Joseph Roque on 2020-03-25.
+//  Copyright © 2020 Joseph Roque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLeaderboardKit.
+FOUNDATION_EXPORT double MyLeaderboardKitVersionNumber;
+
+//! Project version string for MyLeaderboardKit.
+FOUNDATION_EXPORT const unsigned char MyLeaderboardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLeaderboardKit/PublicHeader.h>
+
+
