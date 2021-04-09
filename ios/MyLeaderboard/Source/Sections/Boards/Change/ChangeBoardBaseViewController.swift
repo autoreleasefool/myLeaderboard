d71b0e4c5f9a2024d4b60e377150b17e63c2b5b7
Joseph Roque <github@josephroque.ca>
2021-04-09 00:53:39 -0400

    Force dark mode because it's less ugly

---
+++ ios/MyLeaderboard/Source/Sections/Boards/Change/ChangeBoardBaseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeBoardBaseViewController.swift
+//  MyLeaderboard
+//
+//  Created by Joseph Roque on 2021-04-09.
+//  Copyright © 2021 Joseph Roque. All rights reserved.
+//
+
+import Foundation
---
+++ ios/MyLeaderboard/Source/Sections/Boards/Change/ChangeBoardBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeBoardBuilder.swift
+//  MyLeaderboard
+//
+//  Created by Joseph Roque on 2021-04-08.
+//  Copyright © 2021 Joseph Roque. All rights reserved.
+//
+
+import Foundation
